f2f9217790b60d2a7eb3c46c9bae9f5ac7a2bfb7
Ruhan Khandakar <ruhankhandaker@igmail.com>
2019-02-24 08:54:22 +0530

    Implement Add todo functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,70 @@
 import React, { Component } from "react";
+import {
+    Jumbotron,
+    ListGroup,
+    Container,
+    Row,
+    Col,
+    Button
+} from "react-bootstrap";
+import NavBar from "./components/NavBar";
+import FormComponent from "./components/Form";
 
 class App extends Component {
+    state = {
+        todos: [
+            { id: 0, title: "This is first todo" },
+            { id: 1, title: "This is second todo" }
+        ]
+    };
+    setTodo = todo => {
+        // create id for todo
+        let id, todoLength;
+        todoLength = this.state.todos.length;
+        if (todoLength <= 0) {
+            id = 0;
+        } else {
+            id = this.state.todos[todoLength - 1].id + 1;
+        }
+        const newTodo = {
+            id,
+            title: todo
+        };
+
+        // set new todo to todos
+        this.setState({ todos: [...this.state.todos, newTodo] });
+    };
     render() {
-        return <div className="App" />;
+        return (
+            <React.Fragment>
+                <NavBar />
+                <Jumbotron>
+                    <Container>
+                        <Row>
+                            <Col>
+                                <FormComponent setTodo={this.setTodo} />
+                            </Col>
+                            <Col>
+                                <ListGroup variant="flush">
+                                    {this.state.todos.map(todo => (
+                                        <ListGroup.Item key={todo.id}>
+                                            <Row>
+                                                <Col>{todo.title}</Col>
+                                                <Col>
+                                                    <Button variant="outline-danger">
+                                                        Remove
+                                                    </Button>
+                                                </Col>
+                                            </Row>
+                                        </ListGroup.Item>
+                                    ))}
+                                </ListGroup>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Jumbotron>
+            </React.Fragment>
+        );
     }
 }
 
---
+++ src/components/Form.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { Form, Button } from "react-bootstrap";
+
+export class FormComponent extends Component {
+    state = {
+        todo: ""
+    };
+    handleChange = e => {
+        this.setState({ todo: e.target.value });
+    };
+    handleSubmit = e => {
+        e.preventDefault();
+
+        if (this.state.todo) {
+            this.props.setTodo(this.state.todo);
+        }
+
+        this.setState({ todo: "" });
+    };
+    render() {
+        return (
+            <Form onSubmit={this.handleSubmit}>
+                <Form.Group controlId="add-todo">
+                    <Form.Label>Add Todo</Form.Label>
+                    <Form.Control
+                        placeholder="Add Your todo"
+                        value={this.state.todo}
+                        onChange={this.handleChange}
+                    />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Add
+                </Button>
+            </Form>
+        );
+    }
+}
+
+export default FormComponent;
